1e6d236cc5229eced7f839306b8d7494a961324c
nicolasorellanat <you@example.com>
2020-07-02 21:20:54 -0400

    calculadora basica

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -6,18 +6,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private  string number ="";
-    public double num1 = 0;
-    private doubble num2 = 0;
-    protected String symbol;
+    private String numero ="";
+    private double num1 = 0;
+    private double num2 = 0;
+    private String symbol= "";
 
 
-    private TextView solucion;
-    private Button keyPad5, keyPad8, keyPad3, keyPaddos,
-    private Button keyPadSum, keyPadSubstract, keyPadMultiply, keyPadEqual, equal;
+    private TextView Resultado;
+    private Button key_Pad0, key_Pad1, key_Pad2, key_Pad3, key_Pad4, key_Pad5, key_Pad6, key_Pad7, key_Pad8, key_Pad9;
+    private Button key_PadSubstract, key_PadSum, key_PadMultiply, key_PadDivision, key_PadEqual, key_PadClear;
 
 
     @Override
@@ -26,137 +25,155 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        solucion =  findViewById(R.id.solucion);
+        Resultado =  findViewById(R.id.solucion);
+        //Config Botones
+        key_Pad0 = findViewById(R.id.num_cero);
+        key_Pad1 = findViewById(R.id.num_uno);
+        key_Pad2 = findViewById(R.id.num_dos);
+        key_Pad3 = findViewById(R.id.num_tres);
+        key_Pad4 = findViewById(R.id.num_cuatro);
+        key_Pad5 = findViewById(R.id.num_cinco);
+        key_Pad6 = findViewById(R.id.num_seis);
+        key_Pad7 = findViewById(R.id.num_siete);
+        key_Pad8 = findViewById(R.id.num_ocho);
+        key_Pad9 = findViewById(R.id.num_nueve);
 
-        keyPad5 = findViewById(R.id.num_cuatro);
-        keyPad2 = findViewById(R.id.num_dos);
+        key_PadSubstract = findViewById(R.id.resta);
+        key_PadSum = findViewById(R.id.suma);
+        key_PadMultiply = findViewById(R.id.multiplicacion);
+        key_PadClear = findViewById(R.id.CE);
+        key_PadDivision=findViewById(R.id.division);
+        key_PadEqual=findViewById(R.id.igual);
 
+        //Clicks para efectuar proceso
+        key_Pad0.setOnClickListener(this);
+        key_Pad1.setOnClickListener(this);
+        key_Pad2.setOnClickListener(this);
+        key_Pad3.setOnClickListener(this);
+        key_Pad4.setOnClickListener(this);
+        key_Pad5.setOnClickListener(this);
+        key_Pad6.setOnClickListener(this);
+        key_Pad7.setOnClickListener(this);
+        key_Pad8.setOnClickListener(this);
+        key_Pad9.setOnClickListener(this);
 
-        keyPadSum = findViewById(R.id.sum);
-        keyPadSubstract = findViewById(R.id.division);
-
-        //listener for button
-
-        keyPad5.setOnClickListener(this);
-        keyPad2.setOnClickListener(this);
-
-
-        keyPadSum.setOnClickListener(this);
-        keyPadSubstract.setOnClickListener(this);
+        key_PadSubstract.setOnClickListener(this);
+        key_PadDivision.setOnClickListener(this);
+        key_PadMultiply.setOnClickListener(this);
+        key_PadClear.setOnClickListener(this);
+        key_PadEqual.setOnClickListener(this);
+        key_PadSum.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-
+        //switchs para calculos
         switch (v.getId()){
             case R.id.num_cero:
-                number = number + "0";
-                solucion.setText(number);
+                numero = numero + "0";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_uno:
-                number = number + "1";
-                solucion.setText(number);
+                numero = numero + "1";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_dos:
-                number = number + "";
-                solucion.setText(number);
+                numero = numero + "2";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_tres:
-                number = number + "3";
-                solucion.setText(number);
+                numero = numero + "3";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_cuatro:
-                number = number + "4";
-                solucion.setText(number);
+                numero = numero + "4";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_cinco:
-                number = number + "5";
-                solucion.setText(number);
+                numero = numero + "5";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_seis:
-                number = number + "6";
-                solucion.setText(number);
+                numero = numero + "6";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_siete:
-                number = number + "7";
-                solucion.setText(number);
+                numero  = numero + "7";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_ocho:
-                number = number + "8";
-                solucion.setText(number);
+                numero = numero + "8";
+                Resultado.setText(numero);
                 break;
 
             case R.id.num_nueve:
-                number = number + "9";
-                solucion.setText(number);
+                numero = numero + "9";
+                Resultado.setText(numero);
                 break;
 
-            case R.id.sum:
+            //Operatoria
+            case R.id.suma:
                 symbol = "+";
-                num1 = Integer.parseInt(number);
-                number = "";
+                num1 = Integer.parseInt(numero);
+                numero = "";
                 break;
 
-            case R.id.substract:
+            case R.id.resta:
                 symbol = "-";
-                num1 = Integer.parseInt(number);
-                number = "";
+                num1 = Integer.parseInt(numero);
+                numero = "";
                 break;
 
             case R.id.division:
                 symbol = "/";
-                num1 = Integer.parseInt(number);
-                number = "";
+                num1 = Integer.parseInt(numero);
+                numero = "";
                 break;
 
-            case R.id.multiply:
+            case R.id.multiplicacion:
                 symbol = "X";
-                num1 = Integer.parseInt(number);
-                number = "";
+                num1 = Integer.parseInt(numero);
+                numero = "";
+                break;
+            case R.id.CE:
+                symbol = "";
+                numero = "";
+                num1 = 0;
+                num2= 0;
+                Resultado.setText("");
                 break;
 
-            case R.id.equal:
-                num2 = Integer.parseInt(number);
-
+            case R.id.igual:
+                num2 = Integer.parseInt(numero );
                 switch (symbol){
                     case "+":
-                        solucion.setText( (num1 + num2));
+                        Resultado.setText("solucion: " + (num1 + num2));
                         break;
                     case "-":
-                        solucion.setText("solucion: " + (num1 - num2));
+                        Resultado.setText("solucion: " + (num1 - num2));
                         break;
                     case "/":
-                        solucion.setText("solucion: " + (num1 / num2));
+                        Resultado.setText("solucion: " + (num1 / num2));
                         break;
                     case "X":
-                        solucion.setText("solucion: " + (num1 * num2));
+                        Resultado.setText("solucion: " + (num1 * num2));
                         break;
                 }
 
                 num1 = 0;
                 num2 = 0;
-                number = "";
+                numero = "";
 
                 break;
-
-            case R.id.CE:
-                symbol = "";
-                num1 = 0;
-                num2 = 0;
-                number = "";
-                solucion.setText("");
-                break;
-
-
         }
     }
 }
